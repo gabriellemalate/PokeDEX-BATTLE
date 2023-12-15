308989d8f3c8d79a2e5cea6a379c87b64c5766ed
Gabrielle Malate <gabriellemalate@gmail.com>
2023-12-14 19:16:53 -0800

    second commit

--- scripts/home.js
+++
@@ -1,4 +0,0 @@
-// Standard Character Generator OR Weather Display of 1-3 Planet/s
-
-const weatherApiKey = "b12e8b7e770e9c33falf4c0a424606ed";
-const marvelApiKey = "f9fe640846a7be7fa02f9097a587a8";
